2b55296564eb0ea6e0a5c3eab02816207b94996b
KwonYongBin <dydqls6789@naver.com>
2026-03-08 18:17:40 +0900

    init: react project setup with vite

---
+++ drug-safety-compare/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './styles/global.scss';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>시작</h1>
+    </>
+  )
+}
+
+export default App
